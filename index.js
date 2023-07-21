7c3c07aa45c63a392c74dc289367c82f7b0ade77
numb1086 <numb1086@users.noreply.github.com>
2023-07-21 10:31:01 +0800

    Update index.js
    Update format from ES5 to ES6

--- index.js
+++ index.js
@@ -3,28 +3,28 @@
 
 // init project
 require('dotenv').config();
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
-var cors = require('cors');
+const cors = require('cors');
 app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
 
 // your first API endpoint...
-app.get('/api/hello', function (req, res) {
+app.get('/api/hello', (req, res) => {
   res.json({ greeting: 'hello API' });
 });
 
-app.get('/api/whoami', function(req, res) {
+app.get('/api/whoami', (req, res) => {
   let ip = req.socket.remoteAddress;
   let lang = req.headers["accept-language"];
   let agent = req.headers["user-agent"];
@@ -35,6 +35,6 @@ app.get('/api/whoami', function(req, res) {
 });
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT || 3000, function () {
+const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port);
 });
